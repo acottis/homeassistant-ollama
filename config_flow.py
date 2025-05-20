b7fdec76b8e456ce869628acf3909f0e14b956c6
Adam Cottis <adam.cottis@outlook.com>
2025-05-20 01:01:32 +0100

    Respond with tools sort of

--- config_flow.py
+++ config_flow.py
@@ -20,10 +20,10 @@ _LOGGER = logging.getLogger(__name__)
 AGENT_SCHEMA = Schema(
     {
         Required(
-            CONF_URL, description={"suggested_value": "qwen3:latest"}
+            CONF_MODEL, description={"suggested_value": "qwen3:latest"}
         ): TextSelector(TextSelectorConfig(type=TextSelectorType.URL)),
         Required(
-            CONF_MODEL, description={"suggested_value": "http://localhost:11434"}
+            CONF_URL, description={"suggested_value": "http://localhost:11434"}
         ): str,
     }
 )
--- conversation.py
+++ conversation.py
@@ -1,9 +1,11 @@
 """Integration with Conversation Entity."""
 
+from collections.abc import Mapping, Sequence
 import logging
-from typing import Literal, override
+from typing import Any, Literal, override
 
-from ollama import AsyncClient, RequestError, ResponseError
+from ollama import AsyncClient, Message, RequestError, ResponseError
+import voluptuous_openapi
 
 from homeassistant.components.conversation import ChatLog, ConversationEntity
 from homeassistant.components.conversation.models import (
@@ -13,9 +15,12 @@ from homeassistant.components.conversation.models import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_URL
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import llm
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.intent import IntentResponse
 from homeassistant.util.ssl import get_default_context
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -32,15 +37,35 @@ async def async_setup_entry(
     async_add_entities([LLMAgent(agent)])
 
 
+def add_two_numbers(a: int, b: int) -> int:
+    """Add two numbers
+
+    Args:
+      a (int): The first number
+      b (int): The second number
+
+    Returns:
+      int: The sum of the two numbers
+
+    """
+    return a + b
+
+
 class LLMAgent(ConversationEntity):
     """LLM conversation agent."""
 
     agent: AsyncClient
+    prompt: str
+    apis: str | list[str] | None
+    model: str
 
     def __init__(self, agent: AsyncClient):
         """Init Agent."""
         self._attr_name: str | None = "Custom Agent"
         self.agent = agent
+        self.prompt = "/no_think "
+        self.apis = "assist"
+        self.model = "qwen3:8b"
 
     @property
     @override
@@ -53,13 +78,46 @@ class LLMAgent(ConversationEntity):
         self, user_input: ConversationInput, chat_log: ChatLog
     ) -> ConversationResult:
         _LOGGER.error(user_input)
-        _LOGGER.error(chat_log)
 
+        tools = await self.add_tools(chat_log, user_input)
+
+        messages: Sequence[Mapping[str, Any] | Message] = [
+            {"role": "user", "content": user_input.text}
+        ]
         try:
             response = await self.agent.chat(
-                model="qwen3:8b",
-                messages=[{"role": "user", "content": user_input.text}],
+                model=self.model,
+                messages=messages,
+                tools=tools,
+                stream=False,
             )
+
+            if response.message.tool_calls:
+                for tool in response.message.tool_calls:
+                    print(response.message.tool_calls)
+                    ret = add_two_numbers(**tool.function.arguments)
+                    messages.append(
+                        {
+                            "role": response.message.role,
+                            "content": response.message.role,
+                        }
+                    )
+                    messages.append(
+                        {
+                            "role": "tool",
+                            "name": tool.function.name,
+                            "content": str(ret),
+                        }
+                    )
+                    tool_response = await self.agent.chat(
+                        model=self.model,
+                        messages=messages,
+                        stream=False,
+                    )
+                    response = IntentResponse(language="en")
+                    response.async_set_speech(tool_response.message.content)
+                    return ConversationResult(response)
+
             _LOGGER.error(response)
             message = response.message.content
             if not isinstance(message, str):
@@ -76,3 +134,32 @@ class LLMAgent(ConversationEntity):
         response = IntentResponse(language="en")
         response.async_set_speech(message)
         return ConversationResult(response)
+
+    async def add_tools(
+        self,
+        chat_log: ChatLog,
+        user_input: ConversationInput,
+    ) -> list[dict[str, Any]]:
+        """Insert tools into conversation."""
+        await chat_log.async_update_llm_data(
+            DOMAIN,
+            user_input,
+            self.apis,
+            self.prompt,
+        )
+
+        if chat_log.llm_api:
+            return [ollama_tool(tool) for tool in chat_log.llm_api.tools]
+        return []
+
+
+def ollama_tool(tool: llm.Tool) -> dict[str, Any]:
+    """Covert HA tool into an Ollama Tool."""
+    return {
+        "type": "function",
+        "function": {
+            "name": tool.name,
+            "description": tool.description,
+            "parameters": voluptuous_openapi.convert(tool.parameters),
+        },
+    }
